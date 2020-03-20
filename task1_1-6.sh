90c495d98f35d903eb65f8b49ae0d40472495484
lednah88 <60695556+lednah88@users.noreply.github.com>
2020-03-20 13:11:09 +0200

    Update task1_1-6.sh

--- task1_1-6.sh
+++ task1_1-6.sh
@@ -1,11 +1,11 @@
 recruit@recruit-HP-Compaq-dc7900-Small-Form-Factor:~$ ls
-Desktop  Documents  Downloads  Music  Pictures  Public  Templates  Videos
+Desktop  Documents  Downloads  Music  Pictures  Public  Templates  Videos #this is the list from home/recruit
 recruit@recruit-HP-Compaq-dc7900-Small-Form-Factor:~$ pwd
-/home/recruit
-recruit@recruit-HP-Compaq-dc7900-Small-Form-Factor:~$ mkdir workspace
-recruit@recruit-HP-Compaq-dc7900-Small-Form-Factor:~$ cd workspace
+/home/recruit #this is the location I am at on the system
+recruit@recruit-HP-Compaq-dc7900-Small-Form-Factor:~$ mkdir workspace #I am making a folder or directory called workspace
+recruit@recruit-HP-Compaq-dc7900-Small-Form-Factor:~$ cd workspace #this takes me into the directory called workspace
 recruit@recruit-HP-Compaq-dc7900-Small-Form-Factor:~/workspace$ ls
-recruit@recruit-HP-Compaq-dc7900-Small-Form-Factor:~/workspace$ touch README.md
+recruit@recruit-HP-Compaq-dc7900-Small-Form-Factor:~/workspace$ touch README.md #an empty file named README.md is created
 recruit@recruit-HP-Compaq-dc7900-Small-Form-Factor:~/workspace$ cp README.md CHANGELOG.md
 recruit@recruit-HP-Compaq-dc7900-Small-Form-Factor:~/workspace$ ls
 CHANGELOG.md  README.md
